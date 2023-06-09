e4bf74893d8a3b801b7c97b8be4c7150b2b03dd2
Zackaria Antar <antarzack@gmail.com>
2023-06-09 06:17:53 -0500

    created database, and set up redux imports on indexjs.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from "react-redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
