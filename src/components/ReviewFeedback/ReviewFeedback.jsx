370b0412252fd9c1abd02813c13499ac018eda46
Zackaria Antar <antarzack@gmail.com>
2023-06-10 14:33:12 -0500

    updated feedback display text formatting and styling.

--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -2,31 +2,42 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+function ReviewFeedback() {
+	const inputStore = useSelector((store) => store.inputStore);
+	const postReflection = (event) => {
+		event.preventDefault();
+		axios
+			.post("/feedback", { inputStore })
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-function ReviewFeedback(){
-    const inputStore = useSelector(store => store.inputStore)
-    const postReflection = (event) =>{
-        event.preventDefault()
-        axios.post('/feedback', {inputStore})
-        .then((response)=>{
-            console.log(response);
-        }).catch((err)=>{
-            console.log(err);
-        })
-
-    }
-
-    return (
+	return (
 		<>
 			<h1 className="section-header">
 				Review Feedback Before Submitting
 			</h1>
-			<ul>
-				<li>{inputStore.feeling}</li>
-				<li>{inputStore.understanding}</li>
-				<li>{inputStore.support}</li>
-				<li>{inputStore.comment}</li>
-			</ul>
+			<div className="review-wrapper">
+				<h2>
+					Feeling: <span className="score"> {inputStore.feeling}</span>
+				</h2>
+				<h2>
+					Understanding:
+					<span className="score"> {inputStore.understanding}</span>
+				</h2>
+				<h2>
+					Supported:
+					<span className="score"> {inputStore.support}</span>
+				</h2>
+				<h2>
+					Comments:
+					<span className="score"> {inputStore.comment}</span>
+				</h2>
+			</div>
 			<button className="next-button" onClick={postReflection}>
 				<Link to={"/thanks"}> Submit</Link>
 			</button>
@@ -34,4 +45,4 @@ function ReviewFeedback(){
 	);
 }
 
-export default ReviewFeedback
+export default ReviewFeedback;
