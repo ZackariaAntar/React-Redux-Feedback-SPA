26ef136bfab61e5314744e5dd8084ae7382e7b2f
Zackaria Antar <antarzack@gmail.com>
2023-06-10 14:55:07 -0500

    updated button styling to act differently based on enabled vs disabled.

--- src/components/ThanksPage/ThanksPage.jsx
+++ src/components/ThanksPage/ThanksPage.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 function ThanksPage(){
     return(
         <>
-        <h1>Thanks for your feedback</h1>
-        <button>
+        <div className="FormField-container">
+        <h1>Thanks for your feedback!</h1>
+        <button className="next-button">
             <Link to={'/'}>
                 Submit new feedback
             </Link>
         </button>
+        </div>
         </>
     )
 }
